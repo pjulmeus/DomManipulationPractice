303ddef2e25c97a61d6874c6e5b6073048326a1b
Paul <pjulmeus@gmail.com>
2023-01-12 22:01:40 -0500

    project complete

---
+++ sheet.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+const letter = document.querySelectorAll(".letter");
+for ( let lets of letter){
+setInterval(function(){
+    lets.style.color =randomRGB()
+ },500)
+}
